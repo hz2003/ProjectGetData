860b7a3e7325832d369f41c56aaf1930211bf1e2
hz2003 <418237300hz@163.com>
2015-11-22 16:25:15 -0800

    Update CodeBook

--- CodeBook.md
+++ CodeBook.md
@@ -14,10 +14,24 @@ The dataset includes the following files:
 
 - activity_labels.txt
 
-- test/
+- test/X_test.txt
 
-* X_test.txt
+- test/y_test.txt
 
-* y_test.txt
+- test/subject_test.txt
 
-..* subject_test.txt
+- train/X_train.txt
+
+- train/y_train.txt
+
+- train/subject_train.txt
+
+# Transformation Details
+
+1. Retrieves the test data set and the training data set
+2. Change column names according to features
+3. Extracts the measurements on the mean and standard deviation from features
+4. Appropriately labels the data set with descriptive activity names
+5. Binds the test and training data
+6. Creates tidy data set with the average of each variable for each activity and each subject.
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,30 @@
+# is.installed(x) returns whether x is installed or not
 is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
 
 if(!is.installed("data.table")) {
     install.packages("data.table")
 }
+
+if(!is.installed("reshape2")) {
+    install.packages("reshape2")
+}
+
+packages(data.table)
+packages(reshape2)
+
+# Reading data labels and features
 activity_labels = read.table("./UCI HAR Dataset/activity_labels.txt")[, 2]
 features = read.table("./UCI HAR Dataset/features.txt")
+
+# Find features which contains mean or std
 extract_features = grepl("mean|std", features[,2])
 
+# Reading test data
 X_test = read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test = read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_test = read.table("./UCI HAR Dataset/test/subject_test.txt")
 
+# Changing column name
 names(X_test) = features[, 2]
 X_test = X_test[, extract_features]
 
@@ -19,10 +33,10 @@ y_test[, 2] = activity_labels[y_test[,1]]
 names(y_test) = c("Activity_ID", "Activity_Label")
 names(subject_test) = "subject"
 
-# Bind data
+# Binding data
 test_data = cbind(as.data.table(subject_test), y_test, X_test)
 
-
+# Reading training data
 X_train = read.table("./UCI HAR Dataset/train/X_train.txt")
 y_train = read.table("./UCI HAR Dataset/train/y_train.txt")
 
@@ -35,6 +49,7 @@ y_train[,2] = activity_labels[y_train[,1]]
 names(y_train) = c("Activity_ID", "Activity_Label")
 names(subject_train) = "subject"
 
+# Binding data
 train_data = cbind(as.data.table(subject_train), y_train, X_train)
 
 data = rbind(test_data, train_data)
@@ -44,4 +59,6 @@ data_labels = setdiff(colnames(data), id_labels)
 melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
 
 tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+
+# Output the tidy data to txt file
 write.table(tidy_data, file = "./tidy_data.txt", row.name=FALSE)
